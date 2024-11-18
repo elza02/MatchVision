ab64707252e72f42f8ea0d70a43b717f5f9d8d0b
Zakaria <zakaria.elalaoui742@gmail.com>
2024-11-18 10:43:00 +0100

    securing api_key

--- producer.py
+++ producer.py
@@ -2,12 +2,18 @@ from kafka import KafkaProducer
 import requests
 import json
 
+
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+with open('key.json') as config_file:
+    config = json.load(config_file)
+api_key = config['api_key']
+
 
 def get_match_data():
     # Fetch match data from Football-data API
     url = "https://api.football-data.org/v2/competitions/PL/matches"
-    headers = {'X-Auth-Token': 'YOUR_API_KEY'}
+    headers = {'X-Auth-Token': api_key}
     response = requests.get(url, headers=headers)
     data = response.json()
     return data['matches']
@@ -16,5 +22,16 @@ match_data = get_match_data()
 
 for match in match_data:
     producer.send('premier_league_results', match)
+
+
+def get_standing():
+    # Fetch match data from Football-data API
+    url = "https://api.football-data.org/v4/competitions/PL/standings"
+    headers = {'X-Auth-Token': 'ee34a6b82918405a8490a2b9ead8f21b'}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+
 
 producer.flush()
--- testing.py
+++ testing.py
@@ -1,8 +1,16 @@
 import requests
+import json
+
+
+with open('key.json') as config_file:
+    config = json.load(config_file)
+api_key = config['api_key']
+
+
 def get_match_data():
     # Fetch match data from Football-data API
     url = "https://api.football-data.org/v2/competitions/PL/matches"
-    headers = {'X-Auth-Token': 'ee34a6b82918405a8490a2b9ead8f21b'}
+    headers = {'X-Auth-Token': api_key}
     response = requests.get(url, headers=headers)
     data = response.json()
     return data['matches']
